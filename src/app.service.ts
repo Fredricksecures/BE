068c7c4cbfedebc2b4c03bf0a8f4636ff42abea6
emekoba <70672283+emekoba@users.noreply.github.com>
2023-02-17 08:43:57 +0100

    added timestamp for daily deployment

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'deployment for 17/02/2023';
   }
 }
